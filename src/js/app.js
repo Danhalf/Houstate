cd0f937d7247dc34becbad4c0e3b3ca946aa73ac
Danhalf <danhel@rocketmail.com>
2023-07-08 09:14:46 +0300

    init: phone mask

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,58 @@
-const buttonSelector = document.querySelector('.nav-btn');
-const menuSelector = document.querySelector('.nav-menu');
-const overlayClass = 'nav-overlay'
-const buttonClassOpen = 'button-open'
-const menuClassOpen = 'menu-open'
+window.addEventListener('DOMContentLoaded', () => {
+   const buttonSelector = document.querySelector('.nav-btn');
+   const menuSelector = document.querySelector('.nav-menu');
+   const overlayClass = 'nav-overlay'
+   const buttonClassOpen = 'button-open'
+   const menuClassOpen = 'menu-open'
 
 
-class NavMenu {
+   class NavMenu {
 
-   #isMenuOpen = false;
+      #isMenuOpen = false;
 
-   constructor({ buttonSelector, buttonClassOpen, menuSelector, menuClassOpen, overlayClass }) {
-      this.buttonSelector = buttonSelector
-      this.buttonClassOpen = buttonClassOpen
-      this.menuSelector = menuSelector
-      this.menuClassOpen = menuClassOpen
-      this.overlayClass = overlayClass
-   }
+      constructor({ buttonSelector, buttonClassOpen, menuSelector, menuClassOpen, overlayClass }) {
+         this.buttonSelector = buttonSelector
+         this.buttonClassOpen = buttonClassOpen
+         this.menuSelector = menuSelector
+         this.menuClassOpen = menuClassOpen
+         this.overlayClass = overlayClass
+      }
 
-   #open() {
-      this.buttonSelector.classList.add(this.buttonClassOpen)
-      document.body.classList.add(this.overlayClass)
-      this.#isMenuOpen = true
-   }
+      #open() {
+         this.buttonSelector.classList.add(this.buttonClassOpen)
+         document.body.classList.add(this.overlayClass)
+         this.#isMenuOpen = true
+      }
 
-   #close() {
-      this.buttonSelector.classList.remove(this.buttonClassOpen)
-      document.body.classList.remove(this.overlayClass)
-      this.#isMenuOpen = false
-   }
+      #close() {
+         this.buttonSelector.classList.remove(this.buttonClassOpen)
+         document.body.classList.remove(this.overlayClass)
+         this.#isMenuOpen = false
+      }
 
-   action = () => {
-      if (this.#isMenuOpen) {
-         this.#close()
-      } else {
-         this.#open()
+      action = () => {
+         if (this.#isMenuOpen) {
+            this.#close()
+         } else {
+            this.#open()
+         }
       }
    }
-}
 
-const { action } = new NavMenu(
-   {
-      buttonSelector,
-      buttonClassOpen,
-      menuSelector,
-      menuClassOpen,
-      overlayClass
-   })
+   const { action } = new NavMenu(
+      {
+         buttonSelector,
+         buttonClassOpen,
+         menuSelector,
+         menuClassOpen,
+         overlayClass
+      })
 
-buttonSelector.addEventListener('click', action);
+   buttonSelector.addEventListener('click', action);
+
+
+   const telInputs = document.querySelectorAll('input[type=text]');
+
+   telInputs.forEach(input => intlTelInput(input))
+})
+
